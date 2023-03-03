6799d7432a1d5ccfd5492e5c3383af79c8d1268c
toufiq007 <t.limon2342@gmail.com>
2023-03-04 00:17:41 +0600

    create a navbar with functional menu icon

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ setInterval(() => {
     }
     
 }, 2000);
+
+document.querySelector(".menu").addEventListener('click',()=>{
+    document.querySelectorAll('.target').forEach((item)=>{
+        item.classList.toggle('change')
+    }) 
+    
+})
+
+
+
+
+
+
